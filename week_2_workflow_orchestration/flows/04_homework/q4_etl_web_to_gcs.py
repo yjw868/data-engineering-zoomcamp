7fa99312f13337124abbb548be3cc0f9b19cf0a5
yjw868 <yjwang868@gmail.com>
2023-02-07 22:01:22 +0000

    Q4

--- week_2_workflow_orchestration/flows/04_homework/q4_etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/04_homework/q4_etl_web_to_gcs.py
@@ -47,7 +47,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
     gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.upload_from_path(from_path=path, to_path=Path(*path.parts[2:]))
+    gcs_block.upload_from_path(from_path=path, to_path=Path(*path.parts[3:]))
     return
 
 
