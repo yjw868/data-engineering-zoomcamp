cae92402d903a83d9a8ca770b0f1acc88c182db9
yjw868 <yjwang868@gmail.com>
2023-02-07 09:49:33 +0000

    Add log print

--- week_2_workflow_orchestration/flows/04_homework/q3_etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/04_homework/q3_etl_gcs_to_bq.py
@@ -53,7 +53,7 @@ def etl_gcs_to_bq(color: str, year: int, month: int):
     write_bq(df)
 
 
-@flow()
+@flow(log_prints=True)
 def etl_parent_flow(
     colors: list[str] = ["yellow"],  years: list[int] = [2021], months: list[int] = [1, 2]
 ):
