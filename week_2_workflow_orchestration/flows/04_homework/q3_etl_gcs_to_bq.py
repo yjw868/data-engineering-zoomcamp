13a366e252036dce874de7a168179bf6ad664125
yjw868 <yjwang868@gmail.com>
2023-02-07 12:30:17 +0000

    Test data location

--- week_2_workflow_orchestration/flows/04_homework/q3_etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/04_homework/q3_etl_gcs_to_bq.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -9,6 +10,7 @@ from prefect_gcp.cloud_storage import GcsBucket
 @task(retries=3)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
+    os.makedirs(f"data/{color}")
     file_name = f'{color}_tripdata_{year}-{month:02}.parquet' 
     path =f"./data/{color}/{file_name}" 
     gcs_path = f"opt/prefect/{path}"
